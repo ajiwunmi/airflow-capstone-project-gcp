d19be576f4e34f9bc4639a3046273d2c59ecdcbd
Ajiwunmi <taiwo.it@gmail.com>
2023-10-20 15:28:47 +0100

    remove limit on invoice_number VARCHAR
    Signed-off-by: Ajiwunmi <taiwo.it@gmail.com>

--- dags/ingest_to_db_from_gcp.py
+++ dags/ingest_to_db_from_gcp.py
@@ -79,14 +79,14 @@ with DAG(
     create_schema_query = f"CREATE SCHEMA IF NOT EXISTS {SCHEMA_NAME};"
     create_table_query = f"""
         CREATE TABLE IF NOT EXISTS {SCHEMA_NAME}.{POSTGRES_TABLE_NAME}  (
-            invoice_number VARCHAR(20),
+            invoice_number VARCHAR,
             stock_code VARCHAR(30),
             detail VARCHAR(1000),
             quantity INT,
             invoice_date TIMESTAMP,
             unit_price NUMERIC(8,3),
             customer_id INT,
-            country VARCHAR(20)
+            country VARCHAR(30)
         );
     """
 
