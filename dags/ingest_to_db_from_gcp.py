442a1863861f55aa6ef938efdaf3b67d90013031
Ajiwunmi <taiwo.it@gmail.com>
2023-10-22 00:34:46 +0100

    GoogleCloudStorageToCsvOperator removed
    Signed-off-by: Ajiwunmi <taiwo.it@gmail.com>

--- dags/ingest_to_db_from_gcp.py
+++ dags/ingest_to_db_from_gcp.py
@@ -15,9 +15,9 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.dates import days_ago
 from airflow.utils.trigger_rule import TriggerRule
-from airflow.providers.google.cloud.operators.gcs import GoogleCloudStorageToCsvOperator
+# from airflow.providers.google.cloud.operators.gcs import GoogleCloudStorageToCsvOperator
 
-# from airflow.providers.google.cloud.transfers.gcs_to_local import GCSToLocalFilesystemOperator
+from airflow.providers.google.cloud.transfers.gcs_to_local import GCSToLocalFilesystemOperator
 from datetime import datetime
 import pandas as pd
 from io import StringIO
